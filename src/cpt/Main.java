edabf80435b56a9841d53377764ebdb591c6dbe0
addisonc1 <addison.chan21@ycdsbk12.ca>
2021-01-25 21:38:51 -0500

    added data

---
+++ src/cpt/Main.java
@@ -0,0 +1,30 @@
+package cpt;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.Tab;
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.scene.chart.PieChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.ScatterChart;
+import javafx.scene.chart.XYChart;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
