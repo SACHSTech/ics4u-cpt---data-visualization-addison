d3ab728bce3cf07227ca3df597e6959ab61eed09
addisonc1 <addison.chan21@ycdsbk12.ca>
2021-01-26 00:22:41 -0500

    progress on these files

---
+++ src/cpt/province.java
@@ -0,0 +1,11 @@
+package cpt;
+
+public class province {
+    private int intyear;
+    private String strprovince;
+    private 
+
+    public province(String[] provincedata){
+        this.
+    }
+}
