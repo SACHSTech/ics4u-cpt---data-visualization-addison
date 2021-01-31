7fa419a9eb9cf96f064b8ad37cb555cdcf493998
addisonc1 <addison.chan21@ycdsbk12.ca>
2021-01-30 20:54:09 -0500

    added engelberts gui and added in the graphs to compare

--- src/controller/CompareCountryController.java
+++ src/controller/CompareCountryController.java
@@ -1,17 +1,22 @@
 
 package controller;
-
+import java.io.FileNotFoundException;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.Node;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.BarChart;
 import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
-
+import model.CovidTableModel;
 
 public class CompareCountryController implements Initializable {
 
@@ -26,42 +31,148 @@ public class CompareCountryController implements Initializable {
     @FXML
     private Label totalNoOfCasesLb1;
     @FXML
-    private ComboBox<?> country1CB;
+    private ComboBox<String> country1CB;
     @FXML
-    private ComboBox<?> country2CB;
+    private ComboBox<String> country2CB;
     @FXML
     private Button compareBtn;
     @FXML
     private Pane lineChartPane;
     @FXML
-    private LineChart<?, ?> lineChart;
+    private LineChart<String, Number> lineChart;
+    @FXML
+    private Pane barChartPane;
+    @FXML
+    private BarChart<String,Number> barChart;
+    @FXML
+    private ComboBox<String> graphFilter;
 
     /**
-     * Initializes the controller class.
+     * Initializes the controller class. 
+     * @author Engelbert.Aroozoo
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+    	try {
+			country1CB.setItems(getCountryName());
+			country2CB.setItems(getCountryName());
+		} catch (FileNotFoundException e) {AlertBox(e.toString());}
+    	
+    	country1CB.getSelectionModel().select(getCompareCountry1());
+    	country2CB.getSelectionModel().select(getCompareCountry2());
+    	
+    	country1NameLb.setText(getCompareCountry1());
+    	country2NameLb.setText(getCompareCountry2());
+    	
+    	graphFilter.setItems(graphFilterList);
+    	graphFilter.getSelectionModel().select("LineGraph");
+    	lineChartPane.setVisible(true);
+    	barChartPane.setVisible(false);	
+    	try {
+			drawChart("Line");
+		} catch (FileNotFoundException e) {AlertBox(e.toString());}
     }    
 
     @FXML
     private void backBtnAction(ActionEvent event) {
-        //e
+    	newWindow("MainWindow");
+    	((Node) (event.getSource())).getScene().getWindow().hide();
     }
 
     @FXML
     private void country1CBAction(ActionEvent event) {
-        //e
+    	setCompareCountry1(country1CB.getSelectionModel().getSelectedItem().toString());
     }
 
     @FXML
     private void country2CBAction(ActionEvent event) {
-        //e
+    	setCompareCountry2(country2CB.getSelectionModel().getSelectedItem().toString());
     }
 
     @FXML
     private void compareBtnAction(ActionEvent event) {
-        //e
+    	if(country1CB.getSelectionModel().isEmpty() || country2CB.getSelectionModel().isEmpty()) {
+    		AlertBox("Please Selecte Both Countries To Continue!");
+    		
+    	}else {
+    		try {
+    			country1NameLb.setText(getCompareCountry1());
+    			country2NameLb.setText(getCompareCountry2());
+    			if(graphFilter.getSelectionModel().getSelectedItem().toString().equals("BarGraph")) {
+            		barChartPane.setVisible(true);
+            		lineChartPane.setVisible(false);
+            		drawChart("Bar");
+            	}else {
+            		barChartPane.setVisible(false);
+            		lineChartPane.setVisible(true);
+            		drawChart("Line");
+            	}
+    		} catch (FileNotFoundException e) {
+    			AlertBox(e.toString());
+    		}
+    	}
+    	
     }
     
+    @FXML
+    void grapFilterAction(ActionEvent event)  {
+    	try {
+    		if(graphFilter.getSelectionModel().getSelectedItem().toString().equals("BarGraph")) {
+        		barChartPane.setVisible(true);
+        		lineChartPane.setVisible(false);
+        		drawChart("Bar");
+        	}else {
+        		barChartPane.setVisible(false);
+        		lineChartPane.setVisible(true);
+        		drawChart("Line");
+        	}
+		} catch (FileNotFoundException e) {AlertBox(e.toString());}
+    }
+    /**
+     * drawing the graphs
+     * @author Addison.Chan
+     */
+    private void drawChart(String type) throws FileNotFoundException{
+        XYChart.Series series1 = new XYChart.Series<>();
+        XYChart.Series series2 = new XYChart.Series<>();
+
+        for CovidTableModel data;getCovidData()){
+            if(data.getCountry().equals(getCompareCountry1())){
+                series1.getData().add(new XYChart.Data("Jan",data.getJanCol()));
+                series1.getData().add(new XYChart.Data("Feb", data.getFebCol()));
+    	        series1.getData().add(new XYChart.Data("March", data.getMarCol()));
+    	        series1.getData().add(new XYChart.Data("April", data.getAprCol()));
+    	        series1.getData().add(new XYChart.Data("May", data.getMayCol()));
+    	        series1.getData().add(new XYChart.Data("June", data.getJunCol()));
+    	        series1.getData().add(new XYChart.Data("July", data.getJulCol()));
+    	        series1.getData().add(new XYChart.Data("August", data.getAugCol()));
+    	        series1.getData().add(new XYChart.Data("Sep", data.getSepCol()));
+    	        series1.getData().add(new XYChart.Data("Oct", data.getOctCol()));
+    	        series1.getData().add(new XYChart.Data("Nov", data.getNovCol()));
+    	        series1.getData().add(new XYChart.Data("Dec", data.getDecCol()));
+                }
+            if(data.getCountry().equals(getCompareCountry2())){
+                series2.getData().add(new XYChart.Data("Jan",data.getJanCol()));
+                series2.getData().add(new XYChart.Data("Feb", data.getFebCol()));
+    	        series2.getData().add(new XYChart.Data("March", data.getMarCol()));
+    	        series2.getData().add(new XYChart.Data("April", data.getAprCol()));
+    	        series2.getData().add(new XYChart.Data("May", data.getMayCol()));
+    	        series2.getData().add(new XYChart.Data("June", data.getJunCol()));
+    	        series2.getData().add(new XYChart.Data("July", data.getJulCol()));
+    	        series2.getData().add(new XYChart.Data("August", data.getAugCol()));
+    	        series2.getData().add(new XYChart.Data("Sep", data.getSepCol()));
+    	        series2.getData().add(new XYChart.Data("Oct", data.getOctCol()));
+    	        series2.getData().add(new XYChart.Data("Nov", data.getNovCol()));
+    	        series2.getData().add(new XYChart.Data("Dec", data.getDecCol()));
+                }
+            }
+            series1.setName(getCompareCountry1());
+            series2.setName(getCompareCountry2());
+            if(type.equals("Bar")){
+                barChart.setAnimated(true);
+            }
+        }
+        
+    }
 }
